e03ec2f1d78bceef2592644530194e43b2a26b8a
Ivan Kodrnja <ikodrnja@gmail.com>
2022-11-13 20:36:17 +0100

    building a view for the memory card and impement its flipping

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,18 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        HStack{
+            
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+        
+        
+    }
+}
+
+struct CardView: View {
+   @State var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3)
+                
+                Text("✈️")
+                    .foregroundColor(Color.orange)
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+            }
+                
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
